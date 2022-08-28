605870e59a936903fe5fcb0899de7f05cc92eb29
Paritosh <13868399+paritosh64ce@users.noreply.github.com>
2022-08-28 13:12:39 +0530

    added swagger + app insights

--- netcore-api/netcore-api/Startup.cs
+++ netcore-api/netcore-api/Startup.cs
@@ -25,6 +25,10 @@ namespace netcore_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAppConfiguration(Configuration)
+                    .AddApplicationInsightsTelemetry()
+                    .AddSwagger();
+            
             services.AddCors();
 
             services.AddControllers();
