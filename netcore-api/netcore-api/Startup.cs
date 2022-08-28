f15ddae1d625936e508d3979de2690285fd725be
Paritosh <13868399+paritosh64ce@users.noreply.github.com>
2022-08-28 15:08:13 +0530

    Added swagger + app insights

--- netcore-api/netcore-api/Startup.cs
+++ netcore-api/netcore-api/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace netcore_api
 {
@@ -25,10 +18,10 @@ namespace netcore_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAppConfiguration(Configuration)
-                    .AddApplicationInsightsTelemetry()
-                    .AddSwagger();
-            
+            services.AddApplicationInsightsTelemetry();
+
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Weather API", Version = "v1" }));
+
             services.AddCors();
 
             services.AddControllers();
@@ -41,6 +34,8 @@ namespace netcore_api
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather API"));
             app.UseHttpsRedirection();
             app.UseRouting();
 
